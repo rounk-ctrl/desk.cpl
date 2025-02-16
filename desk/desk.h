83ead84ada589a64262a670a7efd8c021f8abf7c
rounk-ctrl <70931017+rounk-ctrl@users.noreply.github.com>
2025-02-16 12:55:07 +0400

    seperate logic (2)

--- desk/desk.h
+++ desk/desk.h
@@ -2,9 +2,11 @@
 #include "pch.h"
 #include "theme.h"
 
-
 extern HINSTANCE g_hinst;
 extern IThemeManager2* pThemeManager;
 extern IDesktopWallpaper* pDesktopWallpaper;
 extern LPWSTR wallpath;
 extern COLORREF newColor;
+extern int lastpos;
+extern BOOL noWall;
+extern BOOL firstSelect;
