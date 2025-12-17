e773613d5238371b38691b3df2bed052e881ddf6
rounk-ctrl <70931017+rounk-ctrl@users.noreply.github.com>
2025-12-17 18:04:14 +0400

    initial work on adv appearance

--- desk/AppearanceDlgBox.cpp
+++ desk/AppearanceDlgBox.cpp
@@ -7,6 +7,14 @@ using namespace Microsoft::WRL::Details;
 BOOL CAppearanceDlgBox::OnInitDialog(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
 {
 	hElementCombobox = GetDlgItem(1126);
+	hSizeUpdown = GetDlgItem(1133);
+	hColor1 = GetDlgItem(1135);
+	hColor2 = GetDlgItem(1141);
+	hFontCmb = GetDlgItem(1129);
+	hFontSize = GetDlgItem(1130);
+	hFontColor = GetDlgItem(1136);
+	hBold = GetDlgItem(1131);
+	hItalic = GetDlgItem(1132);
 	hPreview = GetDlgItem(1470);
 	size = GetClientSIZE(hPreview);
 
@@ -21,16 +29,21 @@ BOOL CAppearanceDlgBox::OnInitDialog(UINT uMsg, WPARAM wParam, LPARAM lParam, BO
 	HBITMAP hOld = Static_SetBitmap(hPreview, ebmp);
 	if (hOld) DeleteBitmap(hOld);
 
-	for (int i = 1401; i < 1428; ++i)
+	for (int i = 1; i < 10; ++i) // 28
 	{
 		WCHAR szBuffer[40];
-		if (LoadString(g_hThemeUI, i, szBuffer, ARRAYSIZE(szBuffer)))
+		if (LoadString(g_hThemeUI, 1400 + i, szBuffer, ARRAYSIZE(szBuffer)))
 		{
 			int index = ComboBox_AddString(hElementCombobox, szBuffer);
-			ComboBox_SetItemData(hElementCombobox, index, i);
+			ComboBox_SetItemData(hElementCombobox, index, &info[i-1]);
+			
+			if (i == 1)
+			{
+				ComboBox_SetCurSel(hElementCombobox, index);
+				_UpdateControls(&info[i-1]);
+			}
 		}
 	}
-	ComboBox_SetCurSel(hElementCombobox, 6);
 	return TRUE;
 }
 
@@ -46,9 +59,26 @@ LRESULT CAppearanceDlgBox::OnCancel(UINT uNotifyCode, int nID, HWND hWnd, BOOL& 
 	return 0;
 }
 
-void CAppearanceDlgBox::_SetComboboxData()
+BOOL CAppearanceDlgBox::OnComboboxChange(UINT code, UINT id, HWND hWnd, BOOL& bHandled)
 {
+	int index = ComboBox_GetCurSel(hElementCombobox);
+	SCHEMEINFO* tinfo = (SCHEMEINFO*)ComboBox_GetItemData(hElementCombobox, index);
 
+	_UpdateControls(tinfo);
+	return 0;
+}
+
+void CAppearanceDlgBox::_UpdateControls(SCHEMEINFO* info)
+{
+	::EnableWindow(hSizeUpdown, info->activeButton & ACTIVE_SIZEITEM);
+	::EnableWindow((HWND)SendMessage(hSizeUpdown, UDM_GETBUDDY, 0, 0), info->activeButton & ACTIVE_SIZEITEM);
+	::EnableWindow(hColor1, info->activeButton & ACTIVE_COLOR1);
+	::EnableWindow(hColor2, info->activeButton & ACTIVE_COLOR2);
+	::EnableWindow(hFontCmb, info->activeButton & ACTIVE_FONT);
+	::EnableWindow(hFontSize, info->activeButton & ACTIVE_FONT);
+	::EnableWindow(hFontColor, info->activeButton & ACTIVE_FONTCOLOR);
+	::EnableWindow(hBold, info->activeButton & ACTIVE_FONT);
+	::EnableWindow(hItalic, info->activeButton & ACTIVE_FONT);
 }
 
 void CAppearanceDlgBox::OnClose()
--- desk/AppearanceDlgBox.h
+++ desk/AppearanceDlgBox.h
@@ -2,6 +2,31 @@
 #include "pch.h"
 #include "wndprvw.h"
 
+enum ACTIVEBUTTONS
+{
+	ACTIVE_NONE = 1 << 0,
+	ACTIVE_SIZEITEM = 1 << 1,
+	ACTIVE_COLOR1 = 1 << 2,
+	ACTIVE_COLOR2 = 1 << 3,
+	ACTIVE_FONT = 1 << 4,
+	ACTIVE_FONTCOLOR = 1 << 5,
+
+	ACTIVE_ALL = ACTIVE_SIZEITEM | ACTIVE_COLOR1 | ACTIVE_COLOR2 | ACTIVE_FONT | ACTIVE_FONTCOLOR,
+};
+
+DEFINE_ENUM_FLAG_OPERATORS(ACTIVEBUTTONS);
+
+typedef struct tagSCHEMEINFO
+{
+	ACTIVEBUTTONS activeButton;
+	int muiIndex;
+	WORD color1Target;
+	WORD color2Target;
+	WORD fontColorTarget;
+
+	// todo: size
+} SCHEMEINFO;
+
 class CAppearanceDlgBox :
 	public CDialogImpl<CAppearanceDlgBox>
 {
@@ -11,6 +36,7 @@ public:
 private:
 	BEGIN_MSG_MAP(CAppearanceDlgBox)
 		MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+		COMMAND_HANDLER(1126, CBN_SELCHANGE, OnComboboxChange)
 		MSG_WM_CLOSE(OnClose)
 		COMMAND_ID_HANDLER(IDOK, OnOK)
 		COMMAND_ID_HANDLER(IDCANCEL, OnCancel)
@@ -20,11 +46,19 @@ private:
 	void OnClose();
 	LRESULT OnOK(UINT uNotifyCode, int nID, HWND hWnd, BOOL& bHandled);
 	LRESULT OnCancel(UINT uNotifyCode, int nID, HWND hWnd, BOOL& bHandled);
+	BOOL OnComboboxChange(UINT code, UINT id, HWND hWnd, BOOL& bHandled);
 
-	void _SetComboboxData();
-
+	void _UpdateControls(SCHEMEINFO* info);
 
 	HWND hElementCombobox;
+	HWND hSizeUpdown;
+	HWND hColor1;
+	HWND hColor2;
+	HWND hFontCmb;
+	HWND hFontSize;
+	HWND hFontColor;
+	HWND hBold;
+	HWND hItalic;
 	HWND hPreview;
 	SIZE size;
 	Microsoft::WRL::ComPtr<IWindowPreview> pWndPreview;
@@ -43,6 +77,59 @@ private:
 		{
 			WT_MESSAGEBOX,
 			{-75, 64, 75, 64 + 98}
+		}
+	};
+
+	SCHEMEINFO info[28] =
+	{
+		{
+			.activeButton = ACTIVE_COLOR1,
+			.color1Target = COLOR_DESKTOP,
+		},
+		{
+			.activeButton = ACTIVE_ALL,
+			.color1Target = COLOR_INACTIVECAPTION,
+			.color2Target = COLOR_GRADIENTINACTIVECAPTION,
+			.fontColorTarget = COLOR_INACTIVECAPTIONTEXT,
+		},
+		{
+			.activeButton = ACTIVE_SIZEITEM | ACTIVE_COLOR1,
+			.color1Target = COLOR_INACTIVEBORDER,
+		},
+		{
+			.activeButton = ACTIVE_ALL,
+			.color1Target = COLOR_ACTIVECAPTION,
+			.color2Target = COLOR_GRADIENTACTIVECAPTION,
+			.fontColorTarget = COLOR_CAPTIONTEXT,
+		},
+		{
+			.activeButton = ACTIVE_SIZEITEM | ACTIVE_COLOR1,
+			.color1Target = COLOR_ACTIVEBORDER,
+		},
+		{
+			.activeButton = ACTIVE_ALL & ~ACTIVE_COLOR2,
+			.color1Target = COLOR_MENU,
+			.fontColorTarget = COLOR_MENUTEXT,
+		},
+		{
+			.activeButton = ACTIVE_ALL & ~ACTIVE_COLOR2,
+			.color1Target = COLOR_HIGHLIGHT,
+			.fontColorTarget = COLOR_HIGHLIGHTTEXT,
+		},
+		{
+			// window (8)
+			.activeButton = ACTIVE_COLOR1 | ACTIVE_FONTCOLOR,
+			.color1Target = COLOR_WINDOW,
+			.fontColorTarget = COLOR_WINDOWTEXT,
+		},
+		{
+			.activeButton = ACTIVE_SIZEITEM | ACTIVE_COLOR1,
+			.color1Target = COLOR_SCROLLBAR,		// MOD
+		},
+		{
+			.activeButton = ACTIVE_COLOR1 | ACTIVE_FONTCOLOR,
+			.color1Target = COLOR_BTNFACE,
+			.fontColorTarget = COLOR_BTNTEXT,
 		}
 	};
 };
